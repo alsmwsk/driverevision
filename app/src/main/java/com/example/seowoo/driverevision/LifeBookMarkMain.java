b054932ccb79814fc8880c4bf4935033db335636
양서우 <alsmwsk@naver.com>
2018-07-18 19:29:17 +0900

    dd

--- app/src/main/java/com/example/seowoo/driverevision/LifeBookMarkMain.java
+++ app/src/main/java/com/example/seowoo/driverevision/LifeBookMarkMain.java
@@ -2,21 +2,26 @@ package com.example.seowoo.driverevision;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.Permission;
@@ -26,12 +31,13 @@ public class LifeBookMarkMain extends AppCompatActivity {
     private static final String INIT_DATE = "2001-01-01 00:00:00";
     private static final String TAG = "LifeBookMarkMain";
     public static LifeBookMarkMain lifeBookMarkMain = null;
-    private InitAsyncTask initAsyncTask = null;
     private boolean initSettingType = false;
     private Handler pageMoveHandle = null;
     private SharedPreferences pref = null;
     private ProgressDialog progress = null;
     private String today = "";
+    private String result = "";
+    private static boolean network;
 
 
     //메인쓰레드는 ui를 못건드리고 별도의 쓰레드를 이용한다?
@@ -46,11 +52,15 @@ public class LifeBookMarkMain extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
 
+            network = false;
+
+            //"http://alsmwsk3.dothome.co.kr/Android/ScheduleList.php?uuid="
             try{
-                targert = "http://alsmwsk3.dothome.co.kr/Android/ScheduleList.php?uuid=" + URLEncoder.encode(phone,"UTF-8");
+                targert = "http://192.168.219.101:8181/test/androidDB.jsp?phone=" + URLEncoder.encode(phone,"UTF-8");
                 dialog.setMessage("로딩중");
+                dialog.setCancelable(false);
                 dialog.show();
-                wait(3000);
+                //wait(3000);
             }catch (Exception e)
             {
                 e.printStackTrace();
@@ -61,28 +71,45 @@ public class LifeBookMarkMain extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... voids) {
             try{
+
+
                 URL url = new URL(targert);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.connect();
                 //넘어오는 결과값을 저장할수 있도록 하는 메소드..
-                InputStream inputStream = httpURLConnection.getInputStream();
-                //해당 InputStream에 있는 값을 읽을 수 있도록 하기 위한 것
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"utf-8"));
-                String temp;
-                StringBuilder stringBuilder = new StringBuilder();
 
-                //bufferedReader로 읽어온 값을 temp에다 넣은 것이 null이 아니면 temp에다가 값을 직업넣고 개행을해준다.?
-                while ((temp = bufferedReader.readLine()) != null)
-                {
-                    stringBuilder.append(temp + "\n");
+                //jsp와 통신 성공 시 수행
+                if (conn.getResponseCode() == conn.HTTP_OK) {
+                    String temp;
+                    InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(inputStreamReader);
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    // jsp에서 보낸 값을 받는 부분
+                    while ((temp = reader.readLine()) != null) {
+                        stringBuilder.append(temp + "\n");
+                    }
+
+                    reader.close();
+                    inputStreamReader.close();
+                    conn.disconnect();
+
+
+                    return stringBuilder.toString().trim();
+                } else {
+                    // 통신 실패
+                    dialog.dismiss();
+                    Toast.makeText(LifeBookMarkMain.this, "통신 실패", Toast.LENGTH_SHORT).show();
                 }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                return  stringBuilder.toString().trim();
-
-            }catch (Exception e){
-
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
             return null;
         }
 
@@ -99,36 +126,23 @@ public class LifeBookMarkMain extends AppCompatActivity {
 
 
             try {
+                Log.d(TAG, "onPostExecute: result: " + result);
                 JSONObject jsonObject = new JSONObject(result);
-                JSONArray jsonArray = jsonObject.getJSONArray("response");
-                int count = 0;
-                String courseProfessor;
-                String courseTime;
-                String courseTitle;
+                String string = jsonObject.getString("result");
 
-                int courseID;
-                while (count < jsonArray.length())
-                {
-                    JSONObject object = jsonArray.getJSONObject(count);
-                    courseID = object.getInt("courseID");
-                    courseProfessor = object.getString("courseProfessor");
-                    courseTime = object.getString("courseTime");
-                    courseTitle = object.getString("courseTitle");
+                //result = jsonObject.getString("result");
+                //Log.d(TAG, "onPostExecute: result" + string);
 
-                    schedule.addSchedule(courseTime, courseTitle, courseProfessor);
-                    //adapter.notifyDataSetChanged();
-
-                    count++;
-
-                }
 
                 dialog.dismiss();
+                network = true;
+                Toast.makeText(LifeBookMarkMain.this, string, Toast.LENGTH_SHORT)
+                        .show();
 
             }catch (Exception e){
                 e.printStackTrace();;
             }
 
-            schedule.setting(monday, tuesday, wednesday, thursday, friday, getContext());
 
         }
     }
@@ -136,7 +150,9 @@ public class LifeBookMarkMain extends AppCompatActivity {
     // 단말기 핸드폰번호 얻어오기
     public String getPhoneNumber() {
         TelephonyManager mgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        return mgr.getLine1Number();
+
+//        return mgr.getLine1Number();
+        return "010-1234-5678";
 
     }
 
@@ -144,7 +160,14 @@ public class LifeBookMarkMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo);
+        new InitAsyncTask().execute();
 
 
+    }
+
+    public void gosetting(boolean network){
+        if (network){
+            Intent intent = new Intent(this, SettingActivity);
+        }
     }
 }
